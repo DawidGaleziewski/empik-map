f7dea904c8174e49833b9c2414f621774d9a725f
Dawid <dawidgalsino@vp.pl>
2019-12-06 10:09:08 +0100

    added conditional rendering in cases no promotions are found on the teritory

--- src/Components/CarouselMain.js
+++ src/Components/CarouselMain.js
@@ -1,23 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import { Carousel } from 'react-bootstrap';
 import axios from 'axios';
 
 const CarouselMain = ({ merchansideList }) => {
   return (
-    <Carousel>
-      {merchansideList.map(merchanside => {
-        const { id, itemName, imageUrl, description } = merchanside;
-        return (
-          <Carousel.Item key={id.toString()}>
-            <img className="d-block w-100" src={imageUrl} alt={itemName} />
-            <Carousel.Caption>
-              <h3>{itemName}</h3>
-              <p>{description}</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        );
-      })}
-    </Carousel>
+    <section>
+      {merchansideList.length > 0 ? (
+        <Fragment>
+          <h2>Dostępne w Twoim województwie</h2>
+          <Carousel>
+            {merchansideList.map(merchanside => {
+              const { id, itemName, imageUrl, description } = merchanside;
+              return (
+                <Carousel.Item key={id.toString()}>
+                  <img
+                    className="d-block w-100"
+                    src={imageUrl}
+                    alt={itemName}
+                  />
+                  <Carousel.Caption>
+                    <h3>{itemName}</h3>
+                    <p>{description}</p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              );
+            })}
+          </Carousel>
+        </Fragment>
+      ) : (
+        <h2>Brak promocji na tym terenie</h2>
+      )}
+    </section>
   );
 };
 
