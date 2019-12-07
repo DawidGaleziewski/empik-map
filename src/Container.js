d9daa1da7467924f5e8ae12c93c2af783e453f48
Dawid <dawidgalsino@vp.pl>
2019-12-07 21:48:00 +0100

    deploy: update for heroku

--- src/Container.js
+++ src/Container.js
@@ -20,8 +20,13 @@ const Container = () => {
     } = event;
 
     if (id.match(/^(PL-)/)) {
-      document.getElementById(id).classList.toggle('land-toggled');
-      classList.toggle('land-toggled');
+      const UIPreviousLand = document.getElementById(regionState);
+      if (UIPreviousLand) {
+        UIPreviousLand.classList.toggle('land-toggled');
+      }
+
+      const UILand = document.getElementById(id);
+      UILand.classList.toggle('land-toggled');
       setRegionState(id);
       setCarouselIndexState(0);
     }
@@ -43,7 +48,8 @@ const Container = () => {
 
       <main className="main">
         <h1 className="main-header">Empik lokalne promocje</h1>
-        <Map onClick={onClickHandler} />
+        {!regionState ? <Map onClick={onClickHandler} /> : null}
+
         {regionState ? (
           <CarouselMain
             merchansideList={[...merchandiseState]}
